5dfc53b505fb047c9dbdde5f2bce545a848114e4
anuragmishracse <anuragmishracse@gmail.com>
2017-04-28 16:21:43 +0530

    On entire dataset

--- train_model.py
+++ train_model.py
@@ -13,7 +13,7 @@ def train_model(weight = None, batch_size=32, epochs = 10):
     file_name = 'weights-improvement-{epoch:02d}.hdf5'
     checkpoint = ModelCheckpoint(file_name, monitor='loss', verbose=1, save_best_only=True, mode='min')
     callbacks_list = [checkpoint]
-    model.fit_generator(cg.data_generator(batch_size=batch_size), steps_per_epoch=cg.total_samples/batch_size, epochs=2, verbose=2, callbacks=callbacks_list)
+    model.fit_generator(cg.data_generator(batch_size=batch_size), steps_per_epoch=cg.total_samples/batch_size, epochs=epochs, verbose=2, callbacks=callbacks_list)
     try:
         model.save('Models/WholeModel.h5', overwrite=True)
         model.save_weights('Models/Weights.h5',overwrite=True)
@@ -22,4 +22,4 @@ def train_model(weight = None, batch_size=32, epochs = 10):
     print "Training complete...\n"
 
 if __name__ == '__main__':
-    train_model()
+    train_model(epochs=50)
